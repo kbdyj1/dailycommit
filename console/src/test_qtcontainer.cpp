ba7b7c715e415f55456b987a1881babb065d595e
yeongmin.hong <kbdyj@daum.net>
2022-01-17 00:07:28 +0900

    [22/01/17] test QList

--- console/src/test_qtcontainer.cpp
+++ console/src/test_qtcontainer.cpp
@@ -3,7 +3,7 @@
 #include <QDebug>
 #include <QVector>
 
-namespace internal
+namespace internal //==========================================================
 {
 
 const int MAX_COUNT = 1024;
@@ -39,9 +39,59 @@ void testVarLengthArray()
     qDebug() << "QVarLengthArray performance : " << elapsed.elapsed() << "ms";
 }
 
-} //internal
+void testQList()
+{
+    QList<QString> list;
+
+    qDebug() << "QList<QString>.capacity(): " << list.capacity(); // 0
+
+    list.append("one");
+    list.append("two");
+    auto three = QString{"three"};
+    list.append(std::move(three));
+    list.append("two");
+
+    list.emplace(1, 4, 'b');
+
+    list.removeOne("two");
+    //list.resize(8);
+
+    qDebug() << list;
+    qDebug() << three;
+
+    qDebug() << "QList<QString>.capacity(): " << list.capacity(); // 4
+
+    list.squeeze();
+    list.swapItemsAt(0, 1);
+    qDebug() << "squized: " << list << ", capacity: " << list.capacity();
+
+
+}
+
+void testQListImplicitSharing()
+{
+    QList<int> a, b;
+    a.resize(100000);
+
+    auto i = a.begin();
+
+    b = a;
+
+    a[0] = 5;   // a is detached from the shared data.
+                // i is now b's iterator
+
+    b.clear();  // < Qt5.7 : release the allocated memory
+
+    auto j = *i;//undefined behavior !!!
+
+    qDebug() << j;
+}
+
+} //internal ==================================================================
 
 void testQtContinaer()
 {
-    internal::testVarLengthArray();
+    //internal::testVarLengthArray();
+    internal::testQList();
+    //internal::testQListImplicitSharing();
 }
