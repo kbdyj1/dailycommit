513df743fcdf281840982a486f085726c1bd2d11
yeongmin.hong <kbdyj@daum.net>
2022-01-21 00:31:13 +0900

    [22/01/21] test QMetaObject, QMetaType, QMetaEnum

--- quickview/src/test_stateMachine.cpp
+++ quickview/src/test_stateMachine.cpp
@@ -5,12 +5,27 @@
 #include <QState>
 #include <QFinalState>
 
+#ifndef Q_MOC_RUN
+#   define SLOT
+#   define INVOKE
+#endif
+
 class Listener : public QObject
 {
     Q_OBJECT
+    Q_CLASSINFO("author", "yeongmin.hong")
+    Q_CLASSINFO("url", "github.com/kbdyj1")
 
 public:
-    Listener(QObject *button, QObject *parent = nullptr)
+    enum Type {
+        TYPE0,
+        TYPE1,
+        TYPE2,
+        TYPE_MAX
+    };
+    Q_ENUM(Type)
+
+    Q_INVOKABLE Listener(QObject *button, QObject *parent = nullptr)
         : QObject(parent)
     {
         auto *main = new QState();
@@ -44,11 +59,15 @@ public:
         });
     }
 
+    Q_INVOKABLE INVOKE void invokeMethod(int value, QString string) {
+        qDebug() << "invokeMethod(" << value << ", " << string << ")";
+    }
+
 public Q_SLOTS:
-    void print() {
+    SLOT void print() {
         qDebug() << "Listener";
     }
-    void print(QString str) {
+    SLOT void print(QString str) {
         qDebug() << "Listener: " << str;
     }
 
@@ -59,16 +78,57 @@ private:
 void testStateMachine(QQuickView *view)
 {
     view->setSource(QUrl("qrc:/quickview/qml/TestStateMachine.qml"));
-
     auto *rootObject = view->rootObject();
+
     auto *listener = new Listener(rootObject, view);
     const auto *metaObject = listener->metaObject();
+    const auto ci0 = metaObject->classInfo(0);
+    const auto ci1 = metaObject->classInfo(1);
 
     qDebug() << "className: " << metaObject->className();
     qDebug() << "methodOffset: " << metaObject->methodOffset() << ", methodCount: " << metaObject->methodCount();
+    qDebug() << "class info(0) " << ci0.name() << ": " << ci0.value();
+    qDebug() << "class info(1) " << ci1.name() << ": " << ci1.value();
 
     for (auto i = metaObject->methodOffset(); i < metaObject->methodCount(); i++) {
-        qDebug() << metaObject->method(i).methodSignature();
+        qDebug() << metaObject->method(i).methodSignature() << ", tag: " << metaObject->method(i).tag();
+    }
+
+    qDebug() << "constructor ==============================================================";
+    auto constructorCount = metaObject->constructorCount();
+    qDebug() << "constructor count: " << constructorCount;
+    if (constructorCount) {
+        for (auto i=0; i<constructorCount; i++) {
+            auto method = metaObject->constructor(i);
+            qDebug() << "method index: " << method.methodIndex();
+            qDebug() << "method signature: " << method.methodSignature();
+            qDebug() << "name: " << method.name();
+            qDebug() << "parameter count: " << method.parameterCount();
+            qDebug() << "\n";
+        }
+    }
+
+    auto *obj = metaObject->newInstance(Q_ARG(QObject*, rootObject), Q_ARG(QObject*, view));
+
+    QScopedPointer<QObject> p{ obj };
+
+    if (!p.isNull()) {
+        auto * l = dynamic_cast<Listener*>(p.data());
+        if (l) {
+            l->print(QLatin1String("Hello Qt !!!"));
+        }
+    }
+
+    auto metaType = metaObject->metaType();
+    auto id = metaType.fromName("Listener").id();
+    qDebug() << "meta type id: " << id;
+
+    if (metaObject->enumeratorCount()) {
+        auto enum0 = metaObject->enumerator(0);
+        qDebug() << "enum name: " << enum0.enumName();
+        for (auto i=0; i<enum0.keyCount(); i++) {
+            qDebug() << enum0.key(i) << ": " << enum0.value(i);
+        }
     }
 }
 
