5f10733dc6ceb29eecd634c389ae08b1aaa3d143
yeongmin.hong <kbdyj@daum.net>
2022-02-19 22:44:08 +0900

    [22/02/19] test QSettings #2

--- console/src/test_qsettings.cpp
+++ console/src/test_qsettings.cpp
@@ -21,6 +21,20 @@ class Settings : private QSettings
 public:
     using QSettings::QSettings;
 
+    template <Setting::Value key>
+    auto value()
+    {
+        if constexpr (key == Setting::One) {
+            return value(key).toInt();
+        } else if constexpr (key == Setting::Two) {
+            return value(key).toString();
+        } else if constexpr (key == Setting::Three) {
+            return value(key).toDouble();
+        } else if constexpr (key == Setting::Four) {
+            return value(key).toBool();
+        }
+    }
+
     void setValue(Setting::Value key, const QVariant &value)
     {
         const auto meta = QMetaEnum::fromType<Setting::Value>();
@@ -37,10 +51,12 @@ public:
     }
 };
 
+
+
 namespace internal
 {
 
-void test_setting_class()
+void test_setting_using_metaenum()
 {
     Settings settings(QDir::tempPath() + "/daily.ini", QSettings::IniFormat);
     settings.setValue(Setting::One, 1);
@@ -52,6 +68,7 @@ void test_setting_class()
         const auto meta = QMetaEnum::fromType<Setting::Value>();
         const auto text = QString::fromLatin1(meta.valueToKey(key));
         const auto value = settings.value(key);
+
         qDebug() << text << ":" << value;
     };
     print(Setting::One);
@@ -60,11 +77,25 @@ void test_setting_class()
     print(Setting::Four);
 }
 
+void test_setting_using_template()
+{
+    Settings settings(QDir::tempPath() + "/daily.ini", QSettings::IniFormat);
+    settings.setValue(Setting::One, 1);
+    settings.setValue(Setting::Two, "2");
+    settings.setValue(Setting::Three, 3.14);
+    settings.setValue(Setting::Four, true);
+
+    qDebug() << "One :" << settings.value<Setting::One>();
+    qDebug() << "Two :" << settings.value<Setting::Two>();
+    qDebug() << "Three :" << settings.value<Setting::Three>();
+    qDebug() << "Four :" << settings.value<Setting::Four>();
+}
+
 }
 
 void test_qsettings()
 {
-    internal::test_setting_class();
+    internal::test_setting_using_metaenum();
 }
 
 #include "test_qsettings.moc"
