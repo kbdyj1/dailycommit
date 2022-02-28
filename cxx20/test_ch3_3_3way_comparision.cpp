54f51b1c8c7a029403722d4f425052449facb6af
yeongmin.hong <kbdyj@daum.net>
2022-03-01 01:48:24 +0900

    [22/02/28] test user defined 3-way comparision

--- cxx20/test_ch3_3_3way_comparision.cpp
+++ cxx20/test_ch3_3_3way_comparision.cpp
@@ -53,11 +53,9 @@ constexpr bool isLessThan(const T& lhs, const T& rhs)
     return lhs < rhs;
 }
 
-} // namespace
-
-void test_ch3_3_3way_comparision()
+void test0()
 {
-    std::cout << std::boolalpha << std::endl;
+
 #if (0)
     auto m0 = MyInt{2000};
     auto m1 = MyInt{2010};
@@ -71,4 +69,93 @@ void test_ch3_3_3way_comparision()
     auto d1 = MyDouble{2010};
 
     std::cout << "isLessThan(d0, d1) : " << isLessThan(d0, d1) << std::endl;
+}
+
+class MyInt3
+{
+public:
+    constexpr explicit MyInt3(int value) : mValue{value}
+    {}
+    auto operator<=>(const MyInt3 &rhs) const = default;
+
+    constexpr auto operator<=>(const int& rhs) const {
+        return mValue <=> rhs;
+    }
+private:
+    int mValue;
+};
+
+void test1()
+{
+    constexpr auto m0 = MyInt3{2000};
+    constexpr auto m1 = MyInt3{2002};
+    constexpr auto i0 = int{2000};
+    constexpr auto i1 = int{2002};
+
+    auto res = m0 < m1;
+    std::cout << "m0 < m1 : " << res << std::endl;
+
+    res = (m0 <=> m1) < 0;
+    std::cout << "(m0 <=> m1) < 0 : " << res << std::endl;
+
+    res = m0 < i1;
+    std::cout << "m0 < i1 : " << res << std::endl;
+
+    res = (m0 <=> i1) < 0;
+    std::cout << "(m0 <=> i1) < 0 : " << res << std::endl;
+
+    res = i0 < m1;
+    std::cout << "i0 < m1 : " << res << std::endl;
+
+    res = 0 < (i1 <=> m0);
+    std::cout << "0 < (i1 <=> m0) : " << res << std::endl;
+}
+
+class MyInt4
+{
+public:
+    constexpr explicit MyInt4(int value) : mValue{value}
+    {}
+    bool operator==(const MyInt4& rhs) const {
+        std::cout << "== " << std::endl;
+        return mValue == rhs.mValue;
+    }
+    bool operator<(const MyInt4& rhs) const {
+        std::cout << "< " << std::endl;
+        return mValue < rhs.mValue;
+    }
+    auto operator<=>(const MyInt4& rhs) const = default;
+
+private:
+    int mValue;
+};
+
+void test2()
+{
+    auto m0 = MyInt4{2000};
+    auto m1 = MyInt4{2002};
+
+    std::cout << "m0 == m1" << std::endl;
+    m0 == m1;
+    std::cout << "m0 != m1" << std::endl;
+    m0 != m1;
+    std::cout << "m0 < m1" << std::endl;
+    m0 < m1;
+    std::cout << "m0 <= m1" << std::endl;
+    m0 <= m1;
+    std::cout << "m0 > m1" << std::endl;
+    m0 > m1;
+    std::cout << "m0 >= m1" << std::endl;
+    m0 >= m1;
+}
+
+} // namespace
+
+void test_ch3_3_3way_comparision()
+{
+    std::cout << std::boolalpha << std::endl;
+
+    //test0();
+    //test1();
+    test2();
 }
