638a3cbfc8014e615a85938335173b40ab0bbe18
yeongmin.hong <kbdyj@daum.net>
2022-08-03 13:38:58 +0900

    [22/08/03] effective stl #13
    item.22 changing the key of set is prohibited
    item.23 there are cases where a vector is better than an associative container

--- stl/estl_ch_3.cpp
+++ stl/estl_ch_3.cpp
@@ -2,8 +2,21 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+#include "estl_util.h"
 
 namespace { //=================================================================
+
+struct Employee {
+    int id;
+    std::string name;
+    std::string title;
+};
+
+std::ostream& operator<<(std::ostream& os, const Employee& e)
+{
+    os << "employee(" << e.id << ", name: " << e.name << ", title: " << e.title << ")";
+    return os;
+}
 
 namespace item19 {
 
@@ -127,11 +140,6 @@ void test()
 
 namespace item22 {
 
-struct Employee {
-    int id;
-    std::string name;
-    std::string title;
-};
 
 struct IdCmpLess : public std::binary_function<const Employee&, const Employee&, bool>
 {
@@ -140,33 +148,181 @@ struct IdCmpLess : public std::binary_function<const Employee&, const Employee&,
     }
 };
 
-void test()
-{
-    std::set<Employee, IdCmpLess> s;
+std::set<Employee, IdCmpLess> s;
 
+void init()
+{
     s.insert({2, "ccc", "worker"});
     s.insert({1, "bbb", "manager"});
     s.insert({3, "aaa", "vip"});
+}
 
+void print()
+{
+    std::cout << "***** list of set ******\n";
     for (auto iter=s.begin(); iter!=s.end(); iter++) {
         std::cout << "id: " << iter->id << ", name: " << iter->name << ", title: " << iter->title << "\n";
     }
+}
 
+void test_modify_using_cast()
+{
     auto key = 3;
     auto iter = std::find_if(s.begin(), s.end(), [key](const Employee& e) {
         return key == e.id;
     });
     if (iter != s.end()) {
+    #if (1)
         const_cast<Employee&>(*iter).title = "programmer";
+    #else
+        static_cast<Employee>(*iter).title = "programmer";
+    #endif
         std::cout << "id: " << iter->id << ", name: " << iter->name << ", title: " << iter->title << "\n";
     }
+    print();
+}
+
+void test_modify_using_erase()
+{
+    auto key = 3;
+    auto iter = std::find_if(s.begin(), s.end(), [key](const Employee& e) {
+        return key == e.id;
+    });
+    auto employee = *iter;
+    s.erase(iter);
+
+    employee.title = "programmer";
+    s.insert(employee);
+
+    print();
+}
+
+void test()
+{
+    init();
+
+    //test_modify_using_cast();
+    test_modify_using_erase();
 }
 
 } // item22 -----------------------------------------------
+
+namespace item23 {
+
+std::vector<Employee> v;
+
+void init()
+{
+    v.push_back({4, "hhh", "title-18"});
+    v.push_back({2, "ccc", "title-14"});
+    v.push_back({6, "bbb", "title-07"});
+    v.push_back({1, "zzz", "title-10"});
+    v.push_back({5, "aaa", "title-03"});
+
+    std::sort(v.begin(), v.end(), [](const Employee& e0, const Employee& e1){
+        return e0.id < e1.id;
+    });
+}
+
+void test_binary_search(int key)
+{
+    auto e = Employee{key, "", ""};
+    auto less = [](const Employee& e0, const Employee& e1){
+        return e0.id < e1.id;
+    };
+    auto result = std::binary_search(v.begin(), v.end(), e, less);
+    std::cout << "std::binary_search(" << key << "): " << result << "\n";
+}
+
+void test_lower_bound(int key)
+{
+    auto e = Employee{key, "", ""};
+    auto less = [](const Employee& e0, const Employee& e1){
+        return e0.id < e1.id;
+    };
+    auto iter = std::lower_bound(v.begin(), v.end(), e, less);
+    if (iter != v.end()) {
+        std::cout << "std::lower_bound(" << key << "): " << *iter << "\n";
+    } else {
+        std::cout << "std::lower_bound(" << key << "): " << "false\n";
+    }
+}
+
+void test_equal_range()
+{
+    std::vector<int> v0{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    auto pair = std::equal_range(v0.begin(), v0.end(), 4, [](int val0, int val1){
+        return val0 < val1;
+    });
+    for (auto iter=pair.first; iter!=pair.second; iter++) {
+        std::cout << *iter << " ";
+    }
+    std::cout << "\n";
+}
+
+struct Pair {
+    int key;
+    Employee value;
+};
+struct PairCmp {
+    bool keyLess(int l, int r) const
+    {
+        return l < r;
+    }
+    bool operator()(const Pair& p0, const Pair& p1) const {
+        return keyLess(p0.key, p1.key);
+    }
+    bool operator()(const Pair& p, int key) const {
+        return keyLess(p.key, key);
+    }
+    bool operator()(int key, const Pair& p) const {
+        return keyLess(key, p.key);
+    }
+};
+std::ostream& operator<<(std::ostream& os, const Pair& pair)
+{
+    os << "Pair(key: " << pair.key << ", value: " << pair.value << ")";
+    return os;
+}
+
+void test_map_using_vector()
+{
+    std::vector<Pair> m;
+    m.push_back({1, {1, "aaa", "title-1"}});
+    m.push_back({5, {5, "eee", "title-5"}});
+    m.push_back({2, {2, "bbb", "title-2"}});
+    m.push_back({4, {4, "ddd", "title-4"}});
+    m.push_back({3, {3, "ccc", "title-3"}});
+    std::sort(m.begin(), m.end(), PairCmp());
+
+    print_elements(m, "m:\n");
+
+    auto p = Pair{};
+    p.key = 6;
+    auto result = std::binary_search(m.begin(), m.end(), p, PairCmp());
+    std::cout << "std::binary_search(" << p.key << "): " << result << "\n";
+}
+
+void test()
+{
+    init();
+    print_elements(v, "v:\n");
+
+//    test_binary_search(0);
+//    test_binary_search(4);
+
+    //test_lower_bound(3);
+
+    //test_equal_range();
+    test_map_using_vector();
+}
+
+} // item23 -----------------------------------------------
 
 } // namespace ================================================================
 
 void test_ch_3()
 {
-    item22::test();
+    item23::test();
 }
