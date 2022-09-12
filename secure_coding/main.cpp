fd74715770862293a927a8fdcb0e64fc394c184e
yeongmin.hong <kbdyj@daum.net>
2022-09-12 17:39:18 +0900

    [22/09/12] secure coding #1
    string

---
+++ secure_coding/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void test_ch_02();
+
+void printProgramName(const char* name) {
+    size_t len = strlen(name);
+    char* program = new char[len+1];
+    strncpy(program, name, len);
+    program[len] = '\0';
+
+    std::cout << "program: " << program << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    if (0 < argc) {
+        printProgramName(argv[0]);
+    }
+    test_ch_02();
+
+    return 0;
+}
---
+++ secure_coding/util.h
@@ -0,0 +1,36 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <iterator>
+#include <iostream>
+#include <chrono>
+#include <functional>
+
+template <typename C>
+void print(const C& c)
+{
+    std::copy(std::begin(c), std::end(c), std::ostream_iterator<typename C::value_type>(std::cout, " "));
+    std::cout << "\n";
+}
+
+#define STRINGFY2(x)    #x
+#define STRINGFY(x)     STRINGFY2(x)
+#define CONNECT2(x, y)  x##y
+#define CONNECT(x, y)   CONNECT2(x, y)
+
+#define PRINT_FUNC(x)   std::cout << STRINGFY(x) << ": " << x << "\n"
+#define PRINT_ATTR(x)    PRINT_FUNC(x)
+
+template <typename Time = std::chrono::milliseconds, typename Clock = std::chrono::high_resolution_clock>
+struct PerfTimer {
+    template <typename F, typename... Args>
+    static Time duration(F&& func, Args... args) {
+        auto start = Clock::now();
+        std::invoke(std::forward<F>(func), std::forward<Args>(args)...);
+        auto end = Clock::now();
+
+        return std::chrono::duration_cast<Time>(end - start);
+    }
+};
+
+#endif // UTIL_H
