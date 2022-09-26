1bde044fe58b80644490cdf47c1209cc80936c6c
yeongmin.hong <kbdyj@daum.net>
2022-09-26 23:48:57 +0900

    [22/09/26] c++ templates #8
    7. pass by reference or pass by value

--- cpp_templates/ch_07.cpp
+++ cpp_templates/ch_07.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string>
+#include <functional>
 
 using namespace std::string_literals;
 
@@ -94,6 +95,116 @@ void test_out()
 
 } //passByRef -------------------------------------------------------
 
+namespace passByRValue {
+
+template <typename T>
+void pass(T&& arg) {
+    //T x;
+    std::cout << arg << ", [" << typeid(T).name() << "]\n";
+}
+
+void test()
+{
+    pass(62);
+
+    const auto c = "Hi"s;
+    pass(c);
+    pass("Hi");
+    int arr[4];
+    pass(arr);
+}
+
+} //passByRValue ----------------------------------------------------
+
+void printString(const std::string& s)
+{
+    std::cout << "printString(const std::string&)\n";
+}
+
+template <typename T>
+void printT(T arg)
+{
+    printString(arg);
+}
+
+void f(int& n0, int& n1, const int& n2)
+{
+    std::cout << "f(" << n0 << ", " << n1 << ", " << n2 << ")\n";
+    ++n0;
+    ++n1;
+}
+
+void test_cref()
+{
+    auto s = "Hello, Qt"s;
+
+    printT(s);
+    printT(std::cref(s));
+
+    int n0 = 1;
+    int n1 = 2;
+    int n2 = 3;
+
+    auto bound_f = std::function<void()>{ std::bind(f, n0, std::ref(n1), std::cref(n2))};
+    n0 = 10;
+    n1 = 11;
+    n2 = 12;
+
+    std::cout << "before: " << n0 << ", " << n1 << ", " << n2 << "\n";
+
+    bound_f();
+
+    std::cout << "after: " << n0 << ", " << n1 << ", " << n2 << "\n";
+}
+
+template <typename T>
+int compareArray(const T* p0, size_t S0, const T* p1, size_t S1) {
+    for (auto i=0; i<S0 || i<S1; i++) {
+        if (p0[i] < p1[i])
+            return 1;
+        if (p0[i] > p1[i])
+            return -1;
+    }
+    return S0 - S1;
+}
+
+template <typename T, size_t S0, size_t S1>
+void foo(T(&arg0)[S0], T(&arg1)[S1])
+{
+    T* p0 = arg0;
+    T* p1 = arg1;
+    auto ret = compareArray(p0, S0, p1, S1);
+    std::cout << "compareArray(): " << ret << "\n";
+}
+
+void test_array()
+{
+    int array0[] = { 1, 2, 3, 4 };
+    int array1[] = { 1, 2, 3 };
+    int array2[] = { 1, 3, 2 };
+
+    foo(array0, array1);
+    foo(array1, array0);
+    foo(array1, array2);
+}
+
+template <typename T>
+#if (1)
+typename std::remove_reference<T>::type bar(T p)
+#else
+auto bar(T p) //c++14
+#endif
+{
+    std::cout << "bar(" << typeid(T).name() << ")\n";
+    return T{};
+}
+
+void test_return()
+{
+//    int i = 0;
+//    bar<int&>(i);
+}
+
 } //namespace =================================================================
 
 void test_ch_07()
@@ -101,7 +212,10 @@ void test_ch_07()
 #if (0) //done
     passByValue::test();
     passByRef::test();
+    passByRef::test_out();
+    passByRValue::test();
+    test_cref();
 #endif
 
-    passByRef::test_out();
+    test_array();
 }
