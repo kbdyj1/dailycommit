323b56da9e9fcba6c7dea628da246a6dbe72b060
yeongmin.hong <kbdyj@daum.net>
2022-10-13 00:09:08 +0900

    [22/10/12] c++ templates #24
    15. template argument deduction
        decltype(auto)

--- cpp_templates/ch_15_auto.cpp
+++ cpp_templates/ch_15_auto.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+
+int V = 10;
 
 namespace { //=================================================================
 
@@ -44,18 +47,6 @@ struct A
     }
 };
 
-template <typename T>
-struct PMClassT
-{};
-
-template <typename C, typename M>
-struct PMClassT<M C::*> {
-    using Type = C;
-};
-
-template <typename PM>
-using PMClass = typename PMClassT<PM>::Type;
-
 void test()
 {
     auto s = S<int, 16>{};
@@ -65,9 +56,122 @@ void test()
 
 } //_2 --------------------------------------------------------------
 
+namespace _3 {
+
+template <typename>
+struct PMClassT;
+
+template <typename C, typename M>
+struct PMClassT<M C::*> {
+    using Type = C;
+};
+
+template <typename PM>
+using PMClass = typename PMClassT<PM>::Type;
+
+template <auto PMD>
+struct CounterHandle {
+    PMClass<decltype(PMD)>& c;
+    CounterHandle(PMClass<decltype(PMD)>& c) : c(c)
+    {}
+
+    void inc() {
+        ++(c.*PMD);
+
+        std::cout << "CounterHandle::inc() -> " << c.*PMD << "\n";
+    }
+};
+
+struct S {
+    int i;
+};
+
+template <auto... Vs>
+struct Values
+{};
+
+template <auto V, decltype (V)... Vs>
+struct HomogeneousValues
+{};
+
+void g(std::string&& s)
+{
+    std::cout << "std::is_lvalue_reference_v<decltype(s)>" << std::is_lvalue_reference_v<decltype(s)> << "\n";
+    std::cout << "std::is_rvalue_reference_v<decltype(s)>" << std::is_rvalue_reference_v<decltype(s)> << "\n";
+    std::cout << "std::is_same_v<decltype(s), std::string&>" << std::is_same_v<decltype(s), std::string&> << "\n";
+    std::cout << "std::is_same_v<decltype(s), std::string&&>" << std::is_same_v<decltype(s), std::string&&> << "\n";
+    std::cout << "std::is_lvalue_reference_v<decltype((s))>" << std::is_lvalue_reference_v<decltype((s))> << "\n";
+    std::cout << "std::is_rvalue_reference_v<decltype((s))>" << std::is_rvalue_reference_v<decltype((s))> << "\n";
+    std::cout << "std::is_same_v<decltype((s)), std::string&>" << std::is_same_v<decltype((s)), std::string&> << "\n";
+    std::cout << "std::is_same_v<decltype((s)), std::string&&>" << std::is_same_v<decltype((s)), std::string&&> << "\n";
+}
+
+void test()
+{
+    auto s = S{16};
+
+#if 201402L < __cplusplus //c++17
+    auto handle = CounterHandle<&S::i>(s);
+#else
+    auto handle = CounterHandle<int S::*, &S::i>(s);
+#endif
+
+    handle.inc();
+
+    auto v0 = Values<1, 2, 3>{};
+    auto v1 = Values<1, 'x', nullptr>{};
+    auto v2 = HomogeneousValues<1, 2, 3>{};
+#if (0)
+    auto v3 = HomogeneousValues<>{};
+#endif
+
+    g("Hello, Qt");
+}
+
+} //_3 --------------------------------------------------------------
+
+namespace _4 {
+
+void test_decltype_auto()
+{
+    auto i = 10;
+    const auto& r = i;
+    auto x = r;
+    decltype (auto) y = r;
+
+    std::cout << "x: " << typeid(x).name() << ", y: " << typeid(y).name() << "\n";
+
+    auto v = std::vector<int>{1};
+    auto elem0 = v[0];  //int
+    decltype (auto) elem1 = v[0];   //int&
+
+    int k;
+    decltype (auto) kx = k; //int
+    decltype (auto) ky = (k);   //int&
+}
+
+template <decltype (auto) Val>
+class S
+{};
+
+void test()
+{
+    constexpr int C = 10;
+
+    auto s0 = S<C>{};   //S<10>
+    auto s1 = S<(V)>{}; //S<(int&)>
+}
+
+} //_4 --------------------------------------------------------------
+
 } //namespace =================================================================
 
 void test_ch_15_auto()
 {
+#if (0)
     _2::test();
+    _3::test();
+#endif
+
+    _4::test();
 }
