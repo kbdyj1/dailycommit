5d3d34b95f5b2f97c0e6e5528a1058d3965203f2
yeongmin.hong <kbdyj@daum.net>
2022-10-19 01:02:48 +0900

    [22/10/18] c++ templates #30
    16. specialization & overloading
        partial specialization

--- cpp_templates/ch_16_specialization.cpp
+++ cpp_templates/ch_16_specialization.cpp
@@ -128,6 +128,144 @@ constexpr std::size_t SZ<void> = 0;
 
 } //_4 --------------------------------------------------------------
 
+namespace _5 { //full member specialization
+
+template <typename T>
+class Outer {
+    static int code;
+public:
+    template<typename U>
+    class Inner {
+        static int count;
+    };
+
+    void print() const {
+        std::cout << "Outer<T>::print()\n";
+    }
+};
+
+template <typename T>
+int Outer<T>::code = 0;
+
+template <typename T>
+template <typename U>
+int Outer<T>::Inner<U>::count = 0;
+
+template <>
+class Outer<bool> {
+public:
+    template<typename U>
+    class Inner {
+        static int count;
+    };
+
+    void print() const {
+        std::cout << "Outer<bool>::print()\n";
+    }
+};
+
+template <>
+template <typename X>
+class Outer<wchar_t>::Inner {
+public:
+    static long count;
+};
+
+template <>
+template <typename X>
+long Outer<wchar_t>::Inner<X>::count = 10;
+
+template <>
+template <>
+class Outer<char>::Inner<wchar_t> {
+public:
+    enum {
+        count = 5
+    };
+};
+
+void test()
+{
+    auto o0 = Outer<int>{};
+    o0.print();
+
+    auto o1 = Outer<bool>{};
+    o1.print();
+}
+
+class DefaultInitOnly {
+public:
+    DefaultInitOnly() = default;
+    DefaultInitOnly(const DefaultInitOnly&) = delete;
+};
+
+template <typename T>
+class Statics {
+    static T value;
+};
+
+template <>
+DefaultInitOnly Statics<DefaultInitOnly>::value{};
+
+} //_5 --------------------------------------------------------------
+
+namespace _6 {
+
+template <typename T>
+class List {
+public:
+    void append(const T&);
+    inline std::size_t size() const;
+};
+
+template <>
+class List<void*> {
+public:
+    void append(const void*)
+    {}
+    inline std::size_t size() const {
+        return 0;
+    }
+};
+
+template <typename T>
+class List<T*> {
+    List<void*> impl;
+public:
+    inline void append(T* p) {
+        impl.append(p);
+    }
+    inline std::size_t size() const {
+        return impl.size();
+    }
+};
+
+template <typename T, int I = 4>
+class A;
+
+//template <typename T>
+//class A<int, T>;
+
+//template <typename T=int>
+//class A<T, 10>;
+
+//template <int I>
+//class A<int, I*2>;
+
+//template <typename T, int U>
+//class A<U, T>;
+
+template <typename... Ts>
+class Tuple;
+
+//template <typename Tail, typename... Ts>
+//class Tuple<Ts..., Tail>;
+
+template <typename Tail, typename... Ts>
+class Tuple<Tuple<Ts...>, Tail>;
+
+} //_6 --------------------------------------------------------------
+
 } //namespace =================================================================
 
 #include "g.hpp"
@@ -150,5 +288,9 @@ void test()
 
 void test_ch_16_specialization()
 {
+#if (0) //done
     g::test();
+#endif
+
+    _5::test();
 }
