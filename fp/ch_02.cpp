8b6db57060a2e8e82e672ee3f284eeb44c7f81f7
yeongmin.hong <kbdyj@daum.net>
2022-11-23 01:00:12 +0900

    [22/11/22] Functional Programming #2

--- fp/ch_02.cpp
+++ fp/ch_02.cpp
@@ -5,6 +5,28 @@
 
 namespace { //=================================================================
 
+struct person_t {
+    bool female;
+    std::string name;
+};
+
+bool is_female(const person_t& person)
+{
+    return person.female;
+}
+
+bool is_not_female(const person_t& person)
+{
+    return !is_female(person);
+}
+
+std::string name(const person_t& person)
+{
+    return person.name;
+}
+
+auto test_people = std::vector<person_t>{ {true, "Jane"}, {false, "Robert"}, {true, "Minji"}, {true, "Hani"}, {false, "Jason"} };
+
 namespace _1 {
 
 double average_score(const std::vector<int>& scores)
@@ -93,28 +115,6 @@ void test()
 
 namespace _7 {
 
-struct person_t {
-    bool female;
-    std::string name;
-};
-
-
-bool is_female(const person_t& person)
-{
-    return person.female;
-}
-
-bool is_not_female(const person_t& person)
-{
-    return !is_female(person);
-}
-
-std::string name(const person_t& person)
-{
-    return person.name;
-}
-
-
 void test_remove(std::vector<person_t>& v)
 {
     v.erase(std::remove_if(v.begin(), v.end(), is_not_female), v.end());
@@ -131,7 +131,7 @@ std::vector<person_t> test_copy(std::vector<person_t>& v)
 
 void test()
 {
-    auto v = std::vector<person_t>{ {true, "Jane"}, {false, "Robert"}, {true, "Minji"}, {true, "Hani"}, {false, "Jason"} };
+    auto v = test_people;
 
 #if (0)
     test_remove(v);
@@ -154,6 +154,89 @@ void test()
 
 } //_7 --------------------------------------------------------------
 
+namespace _8 {
+
+template <typename FilterFunction>
+std::vector<std::string> names_for(const std::vector<person_t>& people, FilterFunction filter)
+{
+    std::vector<std::string> result;
+
+    for (const auto& person : people) {
+        if (filter(person)) {
+            result.push_back(name(person));
+        }
+    }
+    return result;
+}
+
+template <typename FilterFunction, typename Iterator>
+std::vector<std::string> names_for_helper(Iterator b, Iterator e, FilterFunction filter, std::vector<std::string> prev)
+{
+    if (b == e) {
+        return prev;
+    } else {
+        const auto head = *b;
+        if (filter(head)) {
+            prev.push_back(name(head));
+        }
+
+        return names_for_helper(b+1, e, filter, std::move(prev));
+    }
+}
+
+template <typename FilterFunction, typename Iterator>
+std::vector<std::string> names_for(Iterator b, Iterator e, FilterFunction filter)
+{
+    std::vector<std::string> result;
+
+    return names_for_helper(b, e, filter, result);
+}
+
+bool odd_len(const person_t& person)
+{
+    return person.name.length() % 2 != 0;
+}
+
+void test()
+{
+#if (0)
+    auto res = names_for(test_people, odd_len);
+#else
+    auto res = names_for(test_people.begin(), test_people.end(), odd_len);
+#endif
+
+    for (const auto& s : res) {
+        std::cout << s << '\n';
+    }
+}
+
+} //_8 --------------------------------------------------------------
+
+namespace _9 {
+
+bool filter(const person_t& person)
+{
+    return person.name.length() % 2 != 0;
+}
+
+std::vector<std::string> append_name_if(std::vector<std::string> prev, const person_t& person)
+{
+    if (filter(person)) {
+        prev.push_back(name(person));
+    }
+    return prev;
+}
+
+void test()
+{
+    auto res = std::accumulate(test_people.begin(), test_people.end(), std::vector<std::string>{}, append_name_if);
+    for (const auto& person : res) {
+        std::cout << person << "\n";
+    }
+}
+
+} //_9 --------------------------------------------------------------
+
 void test()
 {
     auto v = std::vector<int>{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
@@ -170,7 +253,9 @@ void test_ch_02()
     test();
     _5::test();
     _6::test();
+    _7::test();
+    _8::test();
 #endif
 
-    _7::test();
+    _9::test();
 }
