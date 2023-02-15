d12ac7f67578c45c90991e9f061bdb220374f778
yeongmin.hong <kbdyj@daum.net>
2023-02-16 01:34:16 +0900

    [23/02/15] linux signal #2
    psignal, strsignal, ...

--- linuxapi2/ch_20.cpp
+++ linuxapi2/ch_20.cpp
@@ -2,18 +2,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+//#define HANDLE_SIGABRT
+#define ENABLE_PSIGNAL
 
 static void sig_int_handler(int sig)
 {
-    fprintf(stdout, "SIGINT[%d] detected\n", sig);
+#if defined(ENABLE_PSIGNAL)
+    psignal(sig, "\tSIGINT");
+#else
+    char* str = strsignal(sig);
+    fprintf(stdout, "\t%s[%d] detected\n", str, sig);
+#endif
+}
+
+static void raise_abort()
+{
+    //deliver SIGABRT to self
+#if (0)
+    kill(0, SIGABRT);
+#else
+    raise(SIGABRT);
+#endif
+}
+
+static bool check_process_exist(int pid)
+{
+    int ret = kill(pid, 0);
+
+    switch (ret) {
+    case ESRCH:
+        return false;
+
+    case EPERM:
+    case 0:
+        return true;
+
+    default:
+        return false;
+    }
 }
 
 static void sig_tstp_handler(int sig)
 {
-    fprintf(stdout, "SIGTSTP[%d] detected\n", sig);
+#if defined(ENABLE_PSIGNAL)
+    psignal(sig, "\tSIGTSTP");
+#else
+    char* str = strsignal(sig);
+    fprintf(stdout, "\t%s[%d] detected\n", str, sig);
+#endif
+
+#ifdef HANDLE_SIGABRT
+    raise_abort();
+#endif
+
+    auto exist = check_process_exist(4003);
+    fprintf(stdout, "qtcreator exist? %d\n", exist);
 }
 
-void test_ch_20()
+void test_ch_20(int argc, const char** argv)
 {
     if (signal(SIGINT, sig_int_handler) == SIG_ERR) {
         fprintf(stderr, "signal(SIGINT) failed.\n");
--- linuxapi2/main.cpp
+++ linuxapi2/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 void test_ch_17();
-void test_ch_20();
+void test_ch_20(int argc, const char** argv);
 
-int main()
+int main(int argc, const char** argv)
 {
     std::cout << std::boolalpha;
 
@@ -11,7 +11,7 @@ int main()
     test_ch_17();
 #endif
 
-    test_ch_20();
+    test_ch_20(argc, argv);
 
     return 0;
 }
