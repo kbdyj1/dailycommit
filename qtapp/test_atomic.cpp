6d22a93e1605a5fa8e93fcf53c1660481b6f9a2b
yeongmin.hong <kbdyj@daum.net>
2023-02-27 00:47:04 +0900

    [23/02/26] Qt atomic
    relaxed, acquire, release, ordered

--- qtapp/test_atomic.cpp
+++ qtapp/test_atomic.cpp
@@ -1,5 +1,15 @@
 #include <QAtomicInteger>
 
+// Memory Order
+//  Relaxed
+//      the compiler and processor to freely reorder memory accesses
+//  Acquire
+//      memory access following the atomic operation may not be re-ordered before the atomic operation
+//  Release
+//      memory access before the atomic operation may not be re-ordered after the atomic operation
+//  Ordered
+//      the same Acquire and Release sematics combined
+
 namespace { //=================================================================
 
 namespace _1 {
