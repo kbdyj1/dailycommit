7c7da17c3ddb81b9d71bc31656af759f045980cd
yeongmin.hong <kbdyj@daum.net>
2023-03-06 01:43:45 +0900

    [23/03/05] process #2
    vfork, parent/child sync

--- linuxapi2/ch_24.cpp
+++ linuxapi2/ch_24.cpp
@@ -112,13 +112,150 @@ void test()
 
 } //_2 --------------------------------------------------------------
 
+namespace _3 {
+
+void test()
+{
+    int iStack = 1;
+
+    switch (vfork()) {
+    case -1:
+        fprintf(stderr, "vfork() error.\n");
+        exit(-1);
+
+    case 0:
+        sleep(2);
+        write(STDOUT_FILENO, "child executing\n", 16);
+        iStack *= 3;
+        _exit(EXIT_SUCCESS);
+
+    default:
+        write(STDOUT_FILENO, "parent executing\n", 17);
+        printf("iStack: %d\n", iStack);
+        exit(EXIT_SUCCESS);
+    }
+}
+
+} //_3 --------------------------------------------------------------
+
+namespace _4 {
+
+void test()
+{
+    int numChildren = 5;
+    int j;
+    pid_t child;
+
+    setbuf(stdout, NULL);
+
+    for (j=0; j<numChildren; j++) {
+        switch (child = fork()) {
+        case -1:
+            fprintf(stderr, "fork() error.\n");
+            exit(-1);
+
+        case 0:
+            printf("[ ] %d\n", j);
+            _exit(EXIT_SUCCESS);
+            break;
+
+        default:
+            printf("[P] %d\n", j);
+            wait(NULL);
+            break;
+        }
+    }
+}
+
+} //_4 --------------------------------------------------------------
+
+namespace _5 {
+
+#ifdef SYNC_SIG
+#undef SYNC_SIG
+#endif
+
+#define SYNC_SIG    SIGUSR1
+
+void handler(int sig)
+{}
+
+void test()
+{
+    pid_t child;
+    sigset_t blockMask, origMask, emptyMask;
+    struct sigaction sa;
+
+    setbuf(stdout, NULL);
+
+    sigemptyset(&blockMask);
+    sigaddset(&blockMask, SYNC_SIG);
+    if (-1 == sigprocmask(SIG_BLOCK, &blockMask, &origMask)) {
+        fprintf(stderr, "sigprocmask() error.\n");
+        exit(-1);
+    }
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sa.sa_handler = handler;
+
+    if (-1 == sigaction(SYNC_SIG, &sa, NULL)) {
+        fprintf(stderr, "sigaction() error.\n");
+        exit(-1);
+    }
+
+    switch (child = fork()) {
+    case -1:
+        fprintf(stderr, "fork() error.\n");
+        exit(-1);
+
+    case 0:
+        printf("[ ] %ld child started.\n", (long)getpid());
+        sleep(2);
+
+        printf("[ ] %ld child about to signal to parent.\n", (long)getpid());
+        if (-1 == kill(getppid(), SYNC_SIG)) {
+            fprintf(stderr, "kill() error.\n");
+            exit(-1);
+        }
+
+        _exit(EXIT_SUCCESS);
+
+    default:
+        printf("[P] %ld parent about to wait for signal.\n", (long)getpid());
+
+        sigemptyset(&emptyMask);
+
+        if (-1 == sigsuspend(&emptyMask) && errno != EINTR) {
+            fprintf(stderr, "sigsuspend() error.\n");
+            exit(-1);
+        }
+
+        printf("[P] %ld parent got signal.\n", (long)getpid());
+
+        if (-1 == sigprocmask(SIG_SETMASK, &origMask, NULL)) {
+            fprintf(stderr, "sigprocmask(origMask) error.\n");
+            exit(-1);
+        }
+
+        exit(EXIT_SUCCESS);
+    }
+}
+
+#undef  SYNC_SIG
+
+} //_5 --------------------------------------------------------------
+
 } //namespace =================================================================
 
 void test_ch_24(int argc, const char* argv[])
 {
 #if (0) //done
     _1::test();
+    _2::test();
+    _3::test();
+    _4::test();
 #endif
 
-    _2::test();
+    _5::test();
 }
