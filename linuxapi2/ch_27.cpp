b4da84b8dffdb84cb824804841c324e68d828646
yeongmin.hong <kbdyj@daum.net>
2023-03-13 23:59:54 +0900

    [23/03/13] exec #4
    system()

--- linuxapi2/ch_27.cpp
+++ linuxapi2/ch_27.cpp
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include "utils.h"
 
 extern char** environ;
@@ -156,12 +160,168 @@ void test(int argc, const char* argv[])
 
 namespace _8 {
 
+void check_shell_enable()
+{
+    int ret = system(NULL);
+
+    printf("shell enable? %s.\n", (ret == 0 ? "false" : "true"));
+}
+
+void check_shell_enable2()
+{
+    int ret = system(":");
+
+    printf(": -> %d.\n", ret);
+}
+
+void check_gxx_version()
+{
+    int ret = system("g++ --version");
+
+    printf("g++ ? %d.\n", ret);
+}
+
+void call_invalid_command()
+{
+    int ret = system("invalid");
+
+    printf("invalid ? 0x%04x (%d, %d).\n", (unsigned int)ret, ret >> 8, ret & 0xff);
+
+    if (-1 == ret)
+        errorExit("system(invalid) error.\n");
+
+    printWaitStatus("system(invalid): ", ret);  //child exited, status=127
+}
+
 void test()
 {
+    //check_shell_enable();
+    check_shell_enable2();
+    //check_gxx_version();
 
+    //call_invalid_command();
 }
 
 } //_8 --------------------------------------------------------------
+
+namespace _9 {
+
+void simple(const char* command)
+{
+    char path[PATH_MAX];
+
+    if (0 == confstr(_CS_PATH, path, PATH_MAX)) {
+        _exit(127);
+    }
+
+    printf("PATH=%s\n", path);
+
+    if (-1 == setenv("PATH", path, 1)) {
+        _exit(127);
+    }
+
+    execlp("sh", "sh", "-c", command, (char*)NULL);
+
+    errorExit("execl() error.\n");
+}
+
+int simulate_system(const char* command)
+{
+    pid_t child;
+    int status;
+
+    switch (child = fork()) {
+    case -1:
+        errorExit("fork() error.\n");
+
+    case 0:
+        execl("/bin/bash", "bash", "-c", command, (char*)NULL);
+        _exit(127);
+        break;
+
+    default:
+        if (-1 == waitpid(child, &status, 0))
+            return -1;
+        else
+            return status;
+    }
+}
+
+int simulate_system2(const char* command)
+{
+    sigset_t blockMask, origMask;
+    struct sigaction saIgnore, saOrigQuit, saOrigInt, saDefault;
+    pid_t child;
+    int status, saveErrno;
+
+    if (NULL == command)
+        return system(":") == 0;
+
+    sigemptyset(&blockMask);
+    sigaddset(&blockMask, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &blockMask, &origMask);
+
+    saIgnore.sa_handler = SIG_IGN;
+    saIgnore.sa_flags = 0;
+    sigemptyset(&saIgnore.sa_mask);
+    sigaction(SIGINT, &saIgnore, &saOrigInt);
+    sigaction(SIGQUIT, &saIgnore, &saOrigQuit);
+
+    switch (child = fork()) {
+    case -1:
+        errorExit("fork() error.\n");
+
+    case 0:
+        saDefault.sa_handler = SIG_DFL;
+        saDefault.sa_flags = 0;
+        sigemptyset(&saDefault.sa_mask);
+
+        if (saOrigInt.sa_handler != SIG_IGN)
+            sigaction(SIGINT, &saDefault, NULL);
+        if (saOrigQuit.sa_handler != SIG_IGN)
+            sigaction(SIGQUIT, &saDefault, NULL);
+
+        sigprocmask(SIG_SETMASK, &origMask, NULL);
+
+        execl("/bin/bash", "bash", "-c", command, (char*)NULL);
+        _exit(127);
+
+    default:
+        while (-1 == waitpid(child, &status, 0)) {
+            if (errno != EINTR) {
+                status = -1;
+                break;
+            }
+        }
+        break;
+    }
+
+    saveErrno = errno;
+
+    sigprocmask(SIG_SETMASK, &origMask, NULL);
+
+    sigaction(SIGINT, &saOrigInt, NULL);
+    sigaction(SIGQUIT, &saOrigQuit, NULL);
+
+    errno = saveErrno;
+
+    return status;
+}
+
+void test()
+{
+    const char* command = "ls | wc";
+
+#if (1)
+    simple(command);
+#else
+    int status =  simulate_system2(command);
+
+    exit(status);
+#endif
+}
+
+} //_9 --------------------------------------------------------------
 
 } //namespace =================================================================
 
@@ -175,7 +335,9 @@ void test_ch_27(int argc, const char* argv[])
     //_5::necho(argc, argv);
     _5::test();
     _6::test();
+    _7::test(argc, argv);
+    _8::test();
 #endif
 
-    _7::test(argc, argv);
+    _9::test();
 }
