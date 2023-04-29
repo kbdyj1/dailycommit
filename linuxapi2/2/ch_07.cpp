eac707a94665c2c2a7f66818534cf63957529044
yeongmin.hong <kbdyj@daum.net>
2023-04-29 10:38:17 +0900

    [23/04/29] PIPE #2

--- linuxapi2/2/ch_07.cpp
+++ linuxapi2/2/ch_07.cpp
@@ -2,8 +2,10 @@
 
 #include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #include "utils.h"
 
@@ -15,7 +17,6 @@ void test()
 {
     int fd[2];
     char buf[32];
-    int size;
 
     if (-1 == pipe(fd)) {
         errorExit("pipe() error.\n");
@@ -28,21 +29,41 @@ void test()
     case 0:
         // close out
         if (-1 == close(fd[1])) {
-            errorExit("close(fd[1]) error.\n");
+            errorExit("CHILD: close(fd[1]) error.\n");
         }
-        size = read(fd[0], buf, 32);
-        buf[size] = '\0';
-        printf("read(%s)\n", buf);
+        for ( ;; ) {
+            int size = read(fd[0], buf, 32);
+            if (-1 == size) {
+                errorExit("CHILD: read() -1\n");
+            } else if (0 == size) {
+                // EOF
+                printf("CHILD: EOF\n");
+                break;
+            }
+            buf[size] = '\0';
+            printf("CHILD: read(%s)\n", buf);
+        }
+        if (-1 == close(fd[0])) {
+            errorExit("CHILD: close(fd[0]) error.\n");
+        }
+        _exit(EXIT_SUCCESS);
         break;
 
     default:
         // close in
         if (-1 == close(fd[0])) {
-            errorExit("close(fd[0]) error.\n");
+            errorExit("PARENT: close(fd[0]) error.\n");
         }
         memset(buf, 0, 32);
         strncpy(buf, "Hello, Qt", 9);
         write(fd[1], buf, 32);
+        if (-1 == close(fd[1])) {
+            errorExit("PARENT: close(fd[1] error.\n");
+        }
+        // wait for the child to exit
+        wait(NULL);
+
+        exit(EXIT_SUCCESS);
         break;
     }
 }
