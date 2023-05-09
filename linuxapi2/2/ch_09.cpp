aa15f0cbe35c08603bdea299b17f6c67e821a982
yeongmin.hong <kbdyj@daum.net>
2023-05-10 01:23:31 +0900

    [23/05/09] System V msgq

--- linuxapi2/2/ch_09.cpp
+++ linuxapi2/2/ch_09.cpp
@@ -1,9 +1,11 @@
+#define __GNU_SOURCE
 #include <sys/types.h>
 #include <sys/msg.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 #include "utils.h"
 
 namespace { //=================================================================
@@ -87,13 +89,103 @@ void test()
 
 } //_2 -------------------------------------------------------------
 
+namespace _3 {
+
+void test()
+{
+    msginfo info;
+    int maxid = msgctl(0, MSG_INFO, (msqid_ds*)&info);
+    if (-1 == maxid) {
+        errorExit("msgctl(MSG_INFO) error.\n");
+    } else {
+        printf("maxind: %d\n\n", maxid);
+    }
+
+    printf("index    id    key    message\n");
+
+    msqid_ds ds;
+    int id;
+    for (int i=0; i<maxid; i++) {
+        id = msgctl(i, MSG_STAT, &ds);
+        if (-1 == id) {
+            if (errno != EINVAL && errno != EACCES) {
+                errorExit("msgctl(MSG_STAT) error.\n");
+            }
+            continue;
+        }
+        printf("%4d %8d 0x%08lx %7ld\n", i, id, (unsigned long)ds.msg_perm.__key, (long)ds.msg_qnum);
+    }
+    exit(EXIT_SUCCESS);
+}
+
+} //_3 -------------------------------------------------------------
+
+namespace _4 {
+
+const int SERVER_KEY = 0x20230509;
+
+const int RESP_MSG_SIZE = 8192;
+
+struct ReqMsg {
+    long type;
+    int pid;
+};
+
+struct ResMsg {
+    long type;
+    char buffer[64];
+};
+
+void test()
+{
+    for (int i=0; i<4; i++) {
+        switch (fork()) {
+        case -1:
+            errorExit("fork() error.\n");
+
+        case 0:
+            {
+                int id = msgget(IPC_PRIVATE, S_IRUSR | S_IWUSR | IPC_CREAT);
+                if (-1 == id) {
+                    errorExit("[client] msgget() error.\n");
+                }
+                ReqMsg msg;
+                msg.type = 1;
+                msg.pid = getpid();
+                if (-1 == msgsnd(id, &msg, sizeof(ReqMsg), 0)) {
+                    errorExit("[client] msgsnd(ClientMsg) error.\n");
+                }
+            }
+            break;
+
+        default:
+            {
+                int id = msgget(IPC_PRIVATE, S_IRUSR | S_IWUSR | IPC_CREAT);
+                if (-1 == id) {
+                    errorExit("[server] msgget() error.\n");
+                }
+
+                int len;
+                ReqMsg msg;
+                for ( ;; ) {
+                    len = msgrcv(id, &msg, sizeof(ReqMsg), 1, 0);
+                }
+            }
+            break;
+        }
+    }
+}
+
+} //_4 -------------------------------------------------------------
+
 } //namespace =================================================================
 
 void exec_ch_09()
 {
 #if (0) //done
     _1::test();
+    _2::test();
 #endif
 
-    _2::test();
+    _3::test();
 }
