6f51fa8d86c63015f418293cf623db04e6ebbe56
yeongmin.hong <kbdyj@daum.net>
2023-05-25 01:01:41 +0900

    [23/05/24] POSIX Message Queue #2

--- linuxapi2/2/ch_15.cpp
+++ linuxapi2/2/ch_15.cpp
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <limits.h>
+#include <unistd.h>
 #include "utils.h"
 
 namespace { //=================================================================
@@ -64,15 +66,153 @@ void test(int argc, const char* argv[])
         errorExit("mq_getattr() failed.\n");
     }
 
-    printf("mq_flags: %d\n", attr.mq_flags);
-    printf("mq_maxmsg: %d\n", attr.mq_maxmsg);
-    printf("mq_msgsize: %d\n", attr.mq_msgsize);
-    printf("mq_curmsgs: %d\n", attr.mq_curmsgs);
+    printf("mq_flags: %ld\n", attr.mq_flags);
+    printf("mq_maxmsg: %ld\n", attr.mq_maxmsg);
+    printf("mq_msgsize: %ld\n", attr.mq_msgsize);
+    printf("mq_curmsgs: %ld\n", attr.mq_curmsgs);
+
+    printf("MQ_PRIO_MAX: %ld\n", sysconf(_SC_MQ_PRIO_MAX));  //32768
 
     exit(EXIT_SUCCESS);
 }
 
 } //_2 --------------------------------------------------------------
+
+namespace _3 {
+
+void test(int argc, const char* argv[])
+{
+    if (argc < 4) {
+        fprintf(stderr, "usage: %s mqid priority text\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    mqd_t id = mq_open(argv[1], O_WRONLY);
+    if (-1 == id) {
+        errorExit("mq_open() error.\n");
+    }
+
+    unsigned int prio = atoi(argv[2]);
+    if (-1 == mq_send(id, argv[3], strlen(argv[3]), prio)) {
+        errorExit("mq_send() error.\n");
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+} //_3 --------------------------------------------------------------
+
+namespace _4 {
+
+void test(int argc, const char* argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s mqid\n", argv[0]);
+    }
+
+    mqd_t id = mq_open(argv[1], O_RDONLY);
+    if (-1 == id) {
+        errorExit("mq_open() failed.\n");
+    }
+
+    mq_attr attr;
+    if (-1 == mq_getattr(id, &attr)) {
+        errorExit("mq_getattr() failed.\n");
+    }
+
+    char* buffer = (char*)malloc(attr.mq_msgsize);
+    if (NULL == buffer) {
+        errorExit("malloc() failed.\n");
+    }
+
+    unsigned int prio;
+    ssize_t reads = mq_receive(id, buffer, attr.mq_msgsize, &prio);
+    if (-1 == reads) {
+        errorExit("mq_receive() error.\n");
+    }
+
+    printf("read: %ld bytes. priority: %u\n", reads, prio);
+    write(STDOUT_FILENO, buffer, reads);
+    write(STDOUT_FILENO, "\n", 1);
+
+    exit(EXIT_SUCCESS);
+}
+
+} //_4 --------------------------------------------------------------
+
+namespace _5 {
+
+#define NOTIFY_SIG  SIGUSR1
+
+void sigHandler(int sig)
+{
+    fprintf(stdout, "sigHandler(%d)\n", sig);
+}
+
+void test(int argc, const char* argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s mq-name.\n", argv[0]);
+    }
+
+    mqd_t id = mq_open(argv[1], O_RDONLY | O_NONBLOCK);
+    if (-1 == id) {
+        errorExit("mq_open() failed.\n");
+    }
+
+    mq_attr attr;
+    if (-1 == mq_getattr(id, &attr)) {
+        errorExit("mq_getattr() failed.\n");
+    }
+
+    char* buffer = (char*)malloc(attr.mq_msgsize);
+    if (NULL == buffer) {
+        errorExit("malloc() failed.\n");
+    }
+
+    sigset_t blockMask;
+    sigemptyset(&blockMask);
+    sigaddset(&blockMask, NOTIFY_SIG);
+    if (-1 == sigprocmask(SIG_BLOCK, &blockMask, NULL)) {
+        errorExit("sigprocaction() failed.\n");
+    }
+
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = sigHandler;
+    if (-1 == sigaction(NOTIFY_SIG, &sa, NULL)) {
+        errorExit("sigaction() failed.\n");
+    }
+
+    sigevent ev;
+    ev.sigev_notify = SIGEV_SIGNAL;
+    ev.sigev_signo = NOTIFY_SIG;
+    if (-1 == mq_notify(id, &ev)) {
+        errorExit("mq_notify() error.\n");
+    }
+
+    sigset_t emptyMask;
+    sigemptyset(&emptyMask);
+
+    ssize_t reads;
+    for ( ;; ) {
+        sigsuspend(&emptyMask);
+
+        if (-1 == mq_notify(id, &ev)) {
+            errorExit("mq_notify() error.\n");
+        }
+
+        while ((reads = mq_receive(id, buffer, attr.mq_msgsize, NULL)) >= 0) {
+            printf("Read %ld bytes.\n", reads);
+        }
+
+        if (errno != EAGAIN) {
+            errorExit("mq_receive() failed.\n");
+        }
+    }
+}
+
+} //_5 --------------------------------------------------------------
 
 } //namespace =================================================================
 
@@ -80,7 +220,10 @@ void exec_ch_15(int argc, const char* argv[])
 {
 #if (0) //done
     _1::test(argc, argv);
+    _2::test(argc, argv);
+    _3::test(argc, argv);
+    _4::test(argc, argv);
 #endif
 
-    _2::test(argc, argv);
+    _5::test(argc, argv);
 }
