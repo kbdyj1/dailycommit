dbf1c28b9c910b8c7a881d5ec54475078cc4af4e
yeongmin.hong <kbdyj@daum.net>
2023-06-06 01:11:56 +0900

    [23/06/05] Socket : Internet Domain #2

--- linuxapi2/2/ch2_22.cpp
+++ linuxapi2/2/ch2_22.cpp
@@ -1,3 +1,6 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
 #include "utils.h"
@@ -47,9 +50,28 @@ ssize_t readLine(int fd, void* buffer, size_t n)
 
 } //_1 --------------------------------------------------------------
 
+namespace _2 {
+
+void test()
+{
+    const char* ip4 = "192.168.0.1";
+    in_addr in;
+    if (0 == inet_aton(ip4, &in)) {
+        errorExit("invalid address\n");
+    }
+
+    printf("%s -> %d\n", ip4, in.s_addr);
+
+    in.s_addr = 16820416;
+    char* p = inet_ntoa(in);
+    printf("%d -> %s\n", in.s_addr, p);
+}
+
+} //_2 --------------------------------------------------------------
+
 } //namespace =================================================================
 
 void exec_ch_22(int argc, const char* argv[])
 {
-
+    _2::test();
 }
