1ef141b4383a9c05eab4cf552d1683162a9b6237
yeongmin.hong <kbdyj@daum.net>
2023-06-07 01:04:26 +0900

    [23/06/06] Socket : Internet Domain #3

--- linuxapi2/2/ch2_22.cpp
+++ linuxapi2/2/ch2_22.cpp
@@ -1,6 +1,10 @@
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <netdb.h>
 #include <unistd.h>
 #include <errno.h>
 #include "utils.h"
@@ -69,9 +73,91 @@ void test()
 
 } //_2 --------------------------------------------------------------
 
+namespace _3 {
+
+void test()
+{
+    addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_protocol = 0;
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    addrinfo* result;
+    int ret = getaddrinfo("qt.io", "443", &hints, &result);
+    if (0 != ret) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+
+    for (addrinfo* info = result; info != NULL; info = result->ai_next) {
+        int s = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+        if (-1 == s)
+            continue;
+
+        printf("socktype: %s\n", info->ai_family == AF_INET ? "IP4" : "IP6");
+        switch (info->ai_family) {
+        case AF_INET:
+            {
+                sockaddr_in* sin = (sockaddr_in*)info->ai_addr;
+                char* p = inet_ntoa(sin->sin_addr);
+                printf("IP4: %s, port: %d\n", p, sin->sin_port);
+            }
+            break;
+
+        case AF_INET6:
+            {
+                char buf[128] = {0, };
+                sockaddr_in6* sin6 = (sockaddr_in6*)info->ai_addr;
+                inet_ntop(AF_INET6, &sin6->sin6_addr, buf, 128);
+                printf("address: %s\n", buf);
+            }
+            break;
+
+        default:
+            fprintf(stderr, "Unknown AF\n");
+            break;
+        }
+
+    }
+}
+
+} //_3 --------------------------------------------------------------
+
+namespace _4 {
+
+void test()
+{
+    sockaddr_in in;
+    memset(&in, 0, sizeof(in));
+    if (0 == inet_aton("52.18.144.254", &in.sin_addr)) {
+        errorExit("invalid address");
+    }
+    in.sin_family = AF_INET;
+    in.sin_port = 47873;
+
+    sockaddr* addr = (sockaddr*)&in;
+    socklen_t addrlen = sizeof(in);
+    char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+    if (0 == getnameinfo(addr, addrlen, hbuf, sizeof(hbuf), sbuf, sizeof(sbuf), NI_NAMEREQD | NI_NUMERICSERV)) {
+        printf("host=%s, serv=%s\n", hbuf, sbuf);
+    }
+}
+
+} //_4 --------------------------------------------------------------
+
 } //namespace =================================================================
 
 void exec_ch_22(int argc, const char* argv[])
 {
+#if (0) //done
     _2::test();
+    _3::test();
+#endif
+
+    _4::test();
 }
