30082039d7b5d5fed6ecb9813cb3b603631504dd
yeongmin.hong <kbdyj@daum.net>
2023-06-17 00:38:09 +0900

    [23/06/16] Terminal #2

--- linuxapi2/2/ch2_25.cpp
+++ linuxapi2/2/ch2_25.cpp
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
+#include <errno.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "utils.h"
 
 namespace { //=================================================================
 
@@ -16,6 +20,24 @@ void print_termios(const char* name, int fd)
     printf("\tc_oflag: %d\n", t.c_oflag);
     printf("\tc_cflag: %d\n", t.c_cflag);
     printf("\tc_lflag: %d\n\n", t.c_lflag);
+
+    printf("\t[%c] Ctrl+C\n", (t.c_cc[VINTR] ? 'v' : ' '));
+    printf("\t[%c] Ctrl+\\\n", (t.c_cc[VQUIT] ? 'v' : ' '));
+    printf("\t[%c] Del\n", (t.c_cc[VERASE] ? 'v' : ' '));
+    printf("\t[%c] @\n", (t.c_cc[VKILL] ? 'v' : ' '));
+    printf("\t[%c] Ctrl+D\n", (t.c_cc[VEOF] ? 'v' : ' '));
+    printf("\t[%c] inter-character timer\n", (t.c_cc[VTIME] ? 'v' : ' '));
+    printf("\t[%c] blocking read until 1 character arrives\n", (t.c_cc[VINTR] ? 'v' : ' '));
+    printf("\t[%c] \\0\n", (t.c_cc[VINTR] ? 'v' : ' '));
+    printf("\t[%c] Ctrl+Q\n", (t.c_cc[VSTART] ? 'v' : ' '));
+    printf("\t[%c] Ctrl+S\n", (t.c_cc[VSTOP] ? 'v' : ' '));
+    printf("\t[%c] Ctrl+Z\n", (t.c_cc[VSUSP] ? 'v' : ' '));
+    printf("\t[%c] EOL\n", (t.c_cc[VEOL] ? 'v' : ' '));
+    printf("\t[%c] Ctrl+R\n", (t.c_cc[VREPRINT] ? 'v' : ' '));
+    printf("\t[%c] Ctrl+U\n", (t.c_cc[VDISCARD] ? 'v' : ' '));
+    printf("\t[%c] Ctrl+W\n", (t.c_cc[VWERASE] ? 'v' : ' '));
+    printf("\t[%c] Ctrl+V\n", (t.c_cc[VLNEXT] ? 'v' : ' '));
+    printf("\t[%c] EOL0\n", (t.c_cc[VEOL2] ? 'v' : ' '));
 }
 
 void test()
@@ -26,16 +48,50 @@ void test()
     int mi = pathconf("/", _PC_MAX_INPUT);
     printf("_PC_MAX_INPUT: %d\n", mi);
 
-    print_termios("STDIN_FILENO", STDIN_FILENO);
-    print_termios("STDOUT_FILENO", STDOUT_FILENO);
-    print_termios("STDERR_FILENO", STDERR_FILENO);
+    print_termios("STDIN_FILENO", STDIN_FILENO);    
 }
 
 } //_1 --------------------------------------------------------------
+
+namespace _2 {
+
+void test(int argc, const char* argv[])
+{
+    int interruptCh;
+    if (1 == argc) {
+        interruptCh = fpathconf(STDIN_FILENO, _PC_VDISABLE);
+        if (-1 == interruptCh) {
+            errnoExit("fpathconf(STDIN_FILENO, _PC_VDISABLE)", errno);
+        }
+    } else if (isdigit(argv[1][0])) {
+        interruptCh = strtoul(argv[1], NULL, 0);
+    } else {
+        interruptCh = argv[1][0];
+    }
+
+    termios ti;
+
+    if (-1 == tcgetattr(STDIN_FILENO, &ti)) {
+        errnoExit("tcgetattr", errno);
+    }
+
+    ti.c_cc[VINTR] = interruptCh;
+
+    if (-1 == tcsetattr(STDIN_FILENO, TCSAFLUSH, &ti)) {
+        errnoExit("tcsetattr", errno);
+    }
+    exit(EXIT_SUCCESS);
+}
+
+} //_2 --------------------------------------------------------------
 
 } //namespace =================================================================
 
 void exec_ch_25(int argc, const char* argv[])
 {
+#if (0) //done
     _1::test();
+#endif
+
+    _2::test(argc, argv);
 }
