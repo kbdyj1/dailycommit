7272b69c69d81a10a53846e72b0ab74e1a66e5d0
yeongmin.hong <kbdyj@daum.net>
2023-06-17 23:28:42 +0900

    [23/06/17] Terminal #3

--- linuxapi2/2/ch2_25.cpp
+++ linuxapi2/2/ch2_25.cpp
@@ -11,6 +11,99 @@ namespace { //=================================================================
 
 namespace _1 {
 
+const char* baudString(int baud)
+{
+    switch (baud) {
+    case B0:    return "hangup";
+    case B50:   return "50";
+    case B75:   return "75";
+    case B110:  return "110";
+    case B134:  return "134";
+    case B150:  return "150";
+    case B200:  return "200";
+    case B300:  return "300";
+    case B600:  return "600";
+    case B1200: return "1200";
+    case B1800: return "1800";
+    case B2400: return "2400";
+    case B4800: return "4800";
+    case B9600: return "9600";
+    case B19200:return "19200";
+    case B38400:return "38400";
+    default:
+        return "Invalid";
+    }
+}
+
+const char* bsDelayString(int delay)
+{
+    switch (delay)
+    {
+    case BS0: return "BS0";
+    case BS1: return "BS1";
+    default:
+        return "Invalid";
+    }
+}
+
+const char* crDelayString(int delay)
+{
+    switch (delay) {
+    case CR0: return "CR0";
+    case CR1: return "CR1";
+    case CR2: return "CR2";
+    case CR3: return "CR3";
+    default:
+        return "Invalid";
+    }
+}
+
+const char* ffDelayString(int delay)
+{
+    switch (delay)
+    {
+    case FF0: return "FF0";
+    case FF1: return "FF1";
+    default:
+        return "Invalid";
+    }
+}
+
+const char* nlDelayString(int delay)
+{
+    switch (delay)
+    {
+    case NL0: return "NL0";
+    case NL1: return "NL1";
+    default:
+        return "Invalid";
+    }
+}
+
+const char* tabDelayString(int delay)
+{
+    switch (delay) {
+    case TAB0: return "TAB0";
+    case TAB1: return "TAB1";
+    case TAB2: return "TAB2";
+    case TAB3: return "TAB3";
+    default:
+        return "Invalid";
+    }
+}
+
+const char* charSizeString(int csize)
+{
+    switch (csize) {
+    case CS5: return "CS5";
+    case CS6: return "CS6";
+    case CS7: return "CS7";
+    case CS8: return "CS8";
+    default:
+        return "Invalid";
+    }
+}
+
 void print_termios(const char* name, int fd)
 {
     termios t;
@@ -37,7 +130,66 @@ void print_termios(const char* name, int fd)
     printf("\t[%c] Ctrl+U\n", (t.c_cc[VDISCARD] ? 'v' : ' '));
     printf("\t[%c] Ctrl+W\n", (t.c_cc[VWERASE] ? 'v' : ' '));
     printf("\t[%c] Ctrl+V\n", (t.c_cc[VLNEXT] ? 'v' : ' '));
-    printf("\t[%c] EOL0\n", (t.c_cc[VEOL2] ? 'v' : ' '));
+    printf("\t[%c] EOL0\n\n", (t.c_cc[VEOL2] ? 'v' : ' '));
+
+    printf("\t[%c] %s\n", t.c_iflag & BRKINT ? 'v' : ' ' , "BREAK -> SIGINT");
+    printf("\t[%c] %s\n", t.c_iflag & ICRNL ? 'v' : ' ', "CR -> NL");
+    printf("\t[%c] %s\n", t.c_iflag & IGNBRK ? 'v' : ' ', "IGNBRK");
+    printf("\t[%c] %s\n", t.c_iflag & IGNCR ? 'v' : ' ', "IGNCR");
+    printf("\t[%c] %s\n", t.c_iflag & IGNPAR ? 'v' : ' ', "IGNPAR (parity error)");
+    printf("\t[%c] %s\n", t.c_iflag & BRKINT ? 'v' : ' ', "IMAX -> SIGINT");
+    printf("\t[%c] %s\n", t.c_iflag & ISTRIP ? 'v' : ' ', "ISTRIP");
+    printf("\t[%c] %s\n", t.c_iflag & IUTF8 ? 'v' : ' ', "IUTF8");
+    printf("\t[%c] %s\n", t.c_iflag & IUCLC ? 'v' : ' ', "IUCLC : convert to lowercase");
+    printf("\t[%c] %s\n", t.c_iflag & IXANY ? 'v' : ' ', "IXANY");
+    printf("\t[%c] %s\n", t.c_iflag & IXOFF ? 'v' : ' ', "IXOFF");
+    printf("\t[%c] %s\n", t.c_iflag & IXON ? 'v' : ' ', "IXON");
+    printf("\t[%c] %s\n\n", t.c_iflag & PARMRK ? 'v' : ' ', "PARMRK : display parity error");
+
+    printf("\t[%s] %s\n", bsDelayString(t.c_oflag & BSDLY), "BSDLY");
+    printf("\t[%s] %s\n", crDelayString(t.c_oflag & CRDLY), "CRDLY");
+    printf("\t[%s] %s\n", ffDelayString(t.c_oflag & FFDLY), "FFDLY form feed");
+    printf("\t[%s] %s\n", nlDelayString(t.c_oflag & NLDLY), "NLDLY");
+    printf("\t[%c] %s\n", t.c_oflag & OCRNL ? 'v' : ' ', "OCRNL");
+    printf("\t[%c] %s\n", t.c_oflag & OFDEL ? 'v' : ' ', "OFDEL : use DEL(0177)");
+    printf("\t[%c] %s\n", t.c_oflag & OFILL ? 'v' : ' ', "OFILL");
+    printf("\t[%c] %s\n", t.c_oflag & OLCUC ? 'v' : ' ', "OLCUC : convert to uppercase");
+    printf("\t[%c] %s\n", t.c_oflag & ONLCR ? 'v' : ' ', "ONLCR");
+    printf("\t[%c] %s\n", t.c_oflag & ONLRET ? 'v' : ' ', "ONLRET");
+    printf("\t[%c] %s\n", t.c_oflag & ONOCR ? 'v' : ' ', "ONOCR");
+    printf("\t[%c] %s\n", t.c_oflag & OPOST ? 'v' : ' ', "OPOST");
+    printf("\t[%s] %s\n", tabDelayString(t.c_oflag & TABDLY), "TABDLY");
+    printf("\t[%c] %s\n\n", t.c_oflag & VTDLY ? 'v' : ' ', "VTDLY (VT0)");
+
+    int baud = t.c_cflag & CBAUD;
+    printf("\t[%s] %s\n", baudString(baud), "CBAUD");
+    printf("\t[%c] %s\n", t.c_cflag & CBAUDEX ? 'v' : ' ', "CBAUDEX");
+    printf("\t[%c] %s\n", t.c_cflag & CIBAUD ? 'v' : ' ', "CIBAUD");
+    printf("\t[%c] %s\n", t.c_cflag & CLOCAL ? 'v' : ' ', "CLOCAL - ignore carrier signal");
+    printf("\t[%c] %s\n", t.c_cflag & CMSPAR ? 'v' : ' ', "CMSPAR - use 'stick' parity");
+    printf("\t[%c] %s\n", t.c_cflag & CREAD ? 'v' : ' ', "CREAD");
+    printf("\t[%c] %s\n", t.c_cflag & CRTSCTS ? 'v' : ' ', "CRTSCTS - enable flow control (RTS/CTS)");
+    printf("\t[%s] %s\n", charSizeString(t.c_cflag & CSIZE), "CIZE");
+    printf("\t[%c] %s\n", t.c_cflag & CSTOPB ? '2' : '1', "CSTOPB - stop bit(s) per char");
+    printf("\t[%c] %s\n", t.c_cflag & HUPCL ? 'v' : ' ', "HUPCL");
+    printf("\t[%c] %s\n", t.c_cflag & PARENB ? 'v' : ' ', "PARENB - enable parity");
+    printf("\t[%c] %s\n\n", t.c_cflag & PARODD ? 'v' : ' ', "PARODD");
+
+    printf("\t[%c] %s\n", t.c_lflag & ECHO ? 'v' : ' ', "ECHO");
+    printf("\t[%c] %s\n", t.c_lflag & ECHOCTL ? 'v' : ' ', "ECHOCTL - (ex: ^L)");
+    printf("\t[%c] %s\n", t.c_lflag & ECHOE ? 'v' : ' ', "ECHOE - Erase");
+    printf("\t[%c] %s\n", t.c_lflag & ECHOK ? 'v' : ' ', "ECHOK - Kill");
+    printf("\t[%c] %s\n", t.c_lflag & ECHOKE ? 'v' : ' ', "ECHOKE");
+    printf("\t[%c] %s\n", t.c_lflag & ECHONL ? 'v' : ' ', "ECHONL");
+    printf("\t[%c] %s\n", t.c_lflag & ECHOPRT ? 'v' : ' ', "ECHOPRT");
+    printf("\t[%c] %s\n", t.c_lflag & FLUSHO ? 'v' : ' ', "FLUSHO");
+    printf("\t[%c] %s\n", t.c_lflag & ICANON ? 'v' : ' ', "ICANON");
+    printf("\t[%c] %s\n", t.c_lflag & IEXTEN ? 'v' : ' ', "IEXTEN");
+    printf("\t[%c] %s\n", t.c_lflag & ISIG ? 'v' : ' ', "ISIG (INTR, QUIT, SUSP)");
+    printf("\t[%c] %s\n", t.c_lflag & NOFLSH ? 'v' : ' ', "NOFLSH (INTR, QUIT, SUSP)");
+    printf("\t[%c] %s\n", t.c_lflag & PENDIN ? 'v' : ' ', "PENDIN");
+    printf("\t[%c] %s\n", t.c_lflag & TOSTOP ? 'v' : ' ', "TOSTOP");
+    printf("\t[%c] %s\n", t.c_lflag & XCASE ? 'v' : ' ', "XCASE");
 }
 
 void test()
@@ -90,8 +242,8 @@ void test(int argc, const char* argv[])
 void exec_ch_25(int argc, const char* argv[])
 {
 #if (0) //done
-    _1::test();
-#endif
-
     _2::test(argc, argv);
+#endif
+    _1::test();
+
 }
