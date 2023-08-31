04ebf3fcd8826ec9f8128ec58b721a7e5d1dbed9
yeongmin.hong <kbdyj@daum.net>
2023-08-31 23:44:50 +0900

    [23/08/31] test QThread #2

--- qtapp/qtcon/test_thread.cpp
+++ qtapp/qtcon/test_thread.cpp
@@ -1,4 +1,5 @@
 #include <QThread>
+#include <QThreadPool>
 #include <QDebug>
 #include <QCoreApplication>
 
@@ -56,11 +57,49 @@ void test(QObject* parent)
 
 } //_1 --------------------------------------------------------------
 
+namespace _2 {
+
+class HelloQtTask : public QRunnable
+{
+    virtual~HelloQtTask()
+    {
+        qDebug() << "~HelloQtTask()";
+    }
+    void run() override
+    {
+        qDebug() << "Hello, Qt";
+    }
+};
+
+void test()
+{
+    QThreadPool* threadPool = QThreadPool::globalInstance();
+
+    qDebug() << "activeThreadCount: " << threadPool->activeThreadCount();
+    qDebug() << "expiryTimeout: " << threadPool->expiryTimeout();
+    qDebug() << "maxThreadCount: " << threadPool->maxThreadCount();
+    qDebug() << "stackSize: " << threadPool->stackSize();
+
+    threadPool->start(new HelloQtTask());
+//    auto func = std::function<void()>([]{
+//        qDebug() << "std::function<()>";
+//    });
+//    threadPool->start(func);
+}
+
+} //_2 --------------------------------------------------------------
+
 } //===========================================================================
 
 void test_thread(QObject* parent)
 {
+    Q_UNUSED(parent)
+
+#if (0) //done
     _1::test(parent);
+#endif
+
+    _2::test();
 }
 
 #include "test_thread.moc"
